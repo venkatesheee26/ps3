0eb757bfd86b2ac545ad0502f17d21c8f30d4413
venkatesheee26@gmail.com <venkatesheee26@gmail.com>
2023-07-12 14:59:00 +0530

    First commit

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ps_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string intString = "123";
+            int intValue;
+            if (int.TryParse(intString, out intValue))
+            {
+                Console.WriteLine("TryParse successful. Converted integer value: " + intValue);
+            }
+            else
+            {
+                Console.WriteLine("TryParse failed. Invalid input string.");
+            }
+
+            // Example using Convert method to convert string to double
+            string doubleString = "3.14";
+            try
+            {
+                double doubleValue = Convert.ToDouble(doubleString);
+                Console.WriteLine("Convert successful. Converted double value: " + doubleValue);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Convert failed. Invalid input format.");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Convert failed. Input value is too large or too small.");
+            }
+
+            // Example using Parse method to convert string to decimal
+            string decimalString = "10.25";
+            try
+            {
+                decimal decimalValue = decimal.Parse(decimalString);
+                Console.WriteLine("Parse successful. Converted decimal value: " + decimalValue);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Parse failed. Invalid input format.");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Parse failed. Input value is too large or too small.");
+
+            }
+        }
+    }
+}
